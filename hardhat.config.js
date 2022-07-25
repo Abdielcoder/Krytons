f4bc71203be41953aaa148cfa99f71a3cae0878b
Abdiel Carrasco <redskullcoder@gmail.com>
2022-07-24 19:13:39 -0700

    KRYTONS DEPLOYED CONTRACT

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
 
-const projecId = process.env.INFURA_PROJECT_ID
-const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
     rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${projecId}`,
+      url: `https://rinkeby.infura.io/v3/${projectId}`,
       accounts: [privateKey],
     },
   },
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,21 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const deploy = async () => {
+    //deployer is for deploy contracts 
+    //from the network previusly configured
+    //Signers get de config information for example private key
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contract Adress: ", deployer.address)
+    //Get data of contract from factory
+    const Kritons = await ethers.getContractFactory("Kritons");
+    //Initializing contract
+    const deployed = await Kritons.deploy();
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    //Get block address
+    console.log("Kritons is deployed at :", deployed.address);
+};
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+//0 = GOOD 1 = WRONG
+deploy().then(() => process.exit(0)).catch(error => {
+    console.log(error);
+    process.exit(1)
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    );
